5b20a2c5e0a669540e4f6d9d302db243209130db
Rigoberto González <themackintux@gmail.com>
2025-02-23 20:13:17 -0600

    feat: initial commit - Spring Boot project setup

---
+++ src/test/java/com/aplazo/bnpl/challenge/AplazoBackendChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aplazo.bnpl.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplazoBackendChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
